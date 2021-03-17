b32d0c20eb14183343c83043f57543bf36043fb3
Isaac Bustamante <izaack.97@gmail.com>
2021-03-17 10:44:26 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+require('colors');
+console.clear();
+
+const main = async () => {
+
+    let opt = '';
+
+    do {
+        opt = await mostrarMenu();
+        console.log({ opt })
+        await pausa();
+    } while ( opt !== '0' );
+};
+
+main();
